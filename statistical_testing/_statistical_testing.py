43c6bc2fa483066ca1511c60aa415cbe0b12ba6f
Alex Moffett <alexander.scott.moffett@gmail.com>
2026-09-10 21:42:05 -0400

    Update _statistical_testing.py

--- statistical_testing/_statistical_testing.py
+++ statistical_testing/_statistical_testing.py
@@ -28,6 +28,7 @@ def reweight_potentials(block_index_name, trait_arrays, path_to_mcmc_trait_data,
         for s in range(n_samples):
             q_rs = calculate_weighted_potential(X[r,:,s], n, params_block, species_labels, tree)
             V[r,s] = -np.log(q_rs)
+    del X
     return block_name, V
 
 def reweight_potentials_parallel(n_processes, trait_arrays, path_to_mcmc_trait_data, params, species_labels, tree):
